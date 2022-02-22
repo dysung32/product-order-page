bc1ecd1d638ac60a3fa073d1a77ab9641cb39486
yoni <sdy32@naver.com>
2022-02-22 15:09:06 +0900

    서버 구현 및 입력 주문 목록을 MongoDB와 연결

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/shopping', methods=['GET'])
+def listing():
+    orders = list(db.orders.find({}, {'_id': False}))
+    return jsonify({'all_orders': orders})
+
+## API 역할을 하는 부분
+@app.route('/shopping', methods=['POST'])
+def saving():
+    name_receive = request.form['name_give']
+    amount_receive = request.form['amount_give']
+    address_receive = request.form['address_give']
+    phonenum_receive = request.form['phonenum_give']
+
+
+    doc = {
+        'name': name_receive,
+        'amount': amount_receive,
+        'address': address_receive,
+        'phonenum': phonenum_receive,
+    }
+    db.orders.insert_one(doc)
+
+    return jsonify({'msg':'주문이 완료되었습니다 !!'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
